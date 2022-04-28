1cc47ffd1c7fa3318b73a1c714293283ef65a9ec
Siripiri-hue <daemonprocellae@live.it>
2022-04-28 19:47:28 +0200

    Recuperati dati dal db

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.standard')
 
 @section('mainContent')
-    
+<main>
+    <h2>Departures</h2>
+    <table>
+        <th>Train code</th>
+        <th>Departure station</th>
+        <th>Arrival station</th>
+        <th>Departure time</th>
+        <th>Arrival time</th>
+        <th>Company</th>
+        @foreach ($data as $item)
+            <tr>
+                <td>{{ $item->train_code }}</td>
+                <td>{{ $item->departure_station }}</td>
+                <td>{{ $item->arrival_station }}</td>
+                <td>{{ $item->departure_time }}</td>
+                <td>{{ $item->arrival_time }}</td>
+                <td>{{ $item->company }}</td>
+            </tr>
+        @endforeach
+    </table>
+</main>
 @endsection
