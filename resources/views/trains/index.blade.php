9d7c940725ab10bc73a134bbade4c117c1ae7c4f
Siripiri-hue <daemonprocellae@live.it>
2022-04-29 12:40:33 +0200

    Corretto html tabella

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,22 +4,28 @@
 <main>
     <table>
         <caption>Departures</caption>
-        <th>Train code</th>
-        <th>Departure station</th>
-        <th>Arrival station</th>
-        <th>Departure time</th>
-        <th>Arrival time</th>
-        <th>Company</th>
-        @foreach ($data as $item)
+        <thead>
             <tr>
-                <td>{{ $item->train_code }}</td>
-                <td>{{ $item->departure_station }}</td>
-                <td>{{ $item->arrival_station }}</td>
-                <td>{{ $item->departure_time }}</td>
-                <td>{{ $item->arrival_time }}</td>
-                <td>{{ $item->company }}</td>
+                <th>Train code</th>
+                <th>Departure station</th>
+                <th>Arrival station</th>
+                <th>Departure time</th>
+                <th>Arrival time</th>
+                <th>Company</th>
             </tr>
-        @endforeach
+        <thead>
+        <tbody>
+            @foreach ($data as $item)
+                <tr>
+                    <td>{{ $item->train_code }}</td>
+                    <td>{{ $item->departure_station }}</td>
+                    <td>{{ $item->arrival_station }}</td>
+                    <td>{{ $item->departure_time }}</td>
+                    <td>{{ $item->arrival_time }}</td>
+                    <td>{{ $item->company }}</td>
+                </tr>
+            @endforeach
+        </tbody>    
     </table>
 </main>
 @endsection
